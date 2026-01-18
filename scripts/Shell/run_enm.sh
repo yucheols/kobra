b92f39321e73641e58887a0dc40472e20b2dd775
Yucheol Shin <brongersmai2@gmail.com>
2026-01-18 03:45:46 -0500

    commit

--- scripts/Shell/run_enm.sh
+++ scripts/Shell/run_enm.sh
@@ -6,15 +6,15 @@
 #SBATCH --mem=350G
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=yshin@amnh.org
-#SBATCH --output=/home/yshin/mendel-nas1/kobra/output/log/cobra_%j-%x.log
-#SBATCH --error=/home/yshin/mendel-nas1/kobra/output/error/cobra_%j-%x.err
+#SBATCH --output=/home/yshin/mendel-nas1/kobra/output/bash_logs/cobra-%x_%j.log
+#SBATCH --error=/home/yshin/mendel-nas1/kobra/output/bash_logs/cobra-%x_%j.err
 
 # load conda in batch mode
 source /home/yshin/mendel-nas1/miniconda3/etc/profile.d/conda.sh
 
 # activate conda environment that contains R and all necessary packages
 conda activate nsdm_hpc
-cd /home/yshin/mendel-nas1/kobra/kobra  # this will be the R working directory on the cluster 
+cd /home/yshin/mendel-nas1/kobra  # this will be the R working directory on the cluster
 
 # run the R script
 Rscript /home/yshin/mendel-nas1/kobra/kobra/scripts/R/enm_script.R
